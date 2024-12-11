da81cf0c47e6725519bf47e726c1c2741ca87f31
Felipe Navas <felipenavas.itec@gmail.com>
2024-12-11 09:53:46 -0300

    Adding and showing data

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js'
-import { } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js"
+import { addDoc, collection, getFirestore, getDocs } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +17,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
+const db = getFirestore()
+
 export const saveTask = (title, description) => {
-  console.log(title, description)
+  addDoc(collection(db, 'tasks'), {title, description})
+}
+
+export const getTasks = () => {
+  return getDocs(collection(db, 'tasks'))
 }
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-import { saveTask } from './firebase.js'
-
-window.addEventListener('DOMContentLoaded', () => {
-
-})
+import { getTasks, saveTask } from './firebase.js'
 
 const taskForm = document.getElementById('task-form')
+const taskContainer = document.getElementById('tasks-container')
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const querySnapshot = await getTasks()
+  let html = ''
+  querySnapshot.forEach((doc) => {
+    const { title, description } = doc.data()
+    html += `
+    <div>
+      <h3>${title}</h3>
+      <p>${description}</p>
+    </div>
+    `
+  })
+  taskContainer.innerHTML = html
+})
 
 taskForm.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -13,4 +25,6 @@ taskForm.addEventListener('submit', (e) => {
   const description = taskForm['task-description']
 
   saveTask(title.value, description.value)
+
+  taskForm.reset()
 })
